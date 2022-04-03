10638c3fd489ad169e8af6e9524cf90a8a69ad62
yash1 <yoyash.p@gmail.com>
2022-04-04 00:09:11 +0530

    'hello'

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request
 import pickle
+import sys
+import logging
 
 model = pickle.load(open('finalmodelchurn.pickle','rb'))
 
 app = Flask(__name__)
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 @app.route('/',methods=['POST','GET'])
 def home():
